56f3bf83aece773fabfdabbd56399f96f4f17914
strongcookdas <goodjav141592@gmail.com>
2022-11-14 23:20:47 +0900

    test : HospitalRepository에 대한 테스트

---
+++ src/test/java/com/example/mini_project_crud_exercise/repository/HospitalRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.example.mini_project_crud_exercise.repository;
+
+import com.example.mini_project_crud_exercise.domian.entity.Hospital;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HospitalRepositoryTest {
+
+    @Autowired
+    HospitalRepository hospitalRepository;
+
+    @Test
+    @DisplayName("hospitalRepository 테스트")
+    void hospitalRepositoryTest() {
+        Optional<Hospital> optionalHospital = hospitalRepository.findById(1);
+        System.out.println(optionalHospital.get().toString());
+    }
+}
