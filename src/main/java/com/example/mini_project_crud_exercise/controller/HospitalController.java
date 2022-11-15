83ba89ca276614ad2da7aec9573a25900e7513f8
strongcookdas <goodjav141592@gmail.com>
2022-11-15 13:06:01 +0900

    feat : 병원 상세정보 페이지 구현

--- src/main/java/com/example/mini_project_crud_exercise/controller/HospitalController.java
+++ src/main/java/com/example/mini_project_crud_exercise/controller/HospitalController.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/hospitals")
@@ -27,5 +29,16 @@ public class HospitalController {
         model.addAttribute("previous",pageable.previousOrFirst().getPageNumber());
         model.addAttribute("next",pageable.next().getPageNumber());
         return "hospitals/list";
+    }
+
+    @GetMapping("/{id}")
+    public String hospitalPage(@PathVariable int id, Model model){
+        Optional<Hospital>optionalHospital = hospitalRepository.findById(id);
+        if(optionalHospital.isEmpty())
+            return "articles/error";
+        else{
+            model.addAttribute("hospital",optionalHospital.get());
+            return "hospitals/show";
+        }
     }
 }
