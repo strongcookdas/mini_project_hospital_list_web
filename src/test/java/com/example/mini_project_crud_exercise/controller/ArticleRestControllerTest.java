cb28a4325ea7a6ca07fc895259cc25ff08432325
strongcookdas <goodjav141592@gmail.com>
2022-11-16 13:13:34 +0900

    feat : ArticleRestController 구현 및 테스트

--- src/test/java/com/example/mini_project_crud_exercise/controller/ArticleRestControllerTest.java
+++ src/test/java/com/example/mini_project_crud_exercise/controller/ArticleRestControllerTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @WebMvcTest(ArticleRestController.class)
 class ArticleRestControllerTest {
